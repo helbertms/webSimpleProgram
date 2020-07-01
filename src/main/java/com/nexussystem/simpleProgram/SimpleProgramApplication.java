fd278782ce79331d32542182a37b6c2e0d49bba6
Helbert Moreira <helbertms@gmail.com>
2020-06-30 22:52:39 -0300

    Create project

---
+++ src/main/java/com/nexussystem/simpleProgram/SimpleProgramApplication.java
@@ -0,0 +1,13 @@
+package com.nexussystem.simpleProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexussystem/simpleProgram/SimpleProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexussystem.simpleProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
